15eb9ba5b5ca81b2d0155ec9a0a95bb18fa02330
Ilram Lima <ilramlima@hotmail.com>
2025-03-31 21:36:12 -0300

    Sintaxe - Desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner teclado = new Scanner(System.in);
+        
+        teclado.useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = teclado.next();
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        int numero = teclado.nextInt();
+        teclado.nextLine();
+        
+        System.out.println("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = teclado.nextLine();
+
+        System.out.println("Por favor, digite o saldo: ");
+        double saldo = teclado.nextDouble();
+
+        teclado.close();
+
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, "
+                + "sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+
+        System.out.println(mensagem);
+
     }
 }
